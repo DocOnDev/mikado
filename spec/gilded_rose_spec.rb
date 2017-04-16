00850aee4078804837d1491fb6007f1ca9f77c5f
Doc Norton <michael@docondev.com>
2017-04-16 14:40:08 -0500

    Remove the useless test

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,11 +3,5 @@ require File.join('./lib', 'gilded_rose')
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      gr = GildedRose.new(items)
-      gr.update_quality()
-      expect(items[0].name).to eq "foo"
-    end
   end
 end
