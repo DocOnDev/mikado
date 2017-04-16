76e6690c6eb72bfa5ad782d967ae023f8af60292
Doc Norton <michael@docondev.com>
2017-04-16 14:46:02 -0500

    decrement standard quality by one

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,5 +3,17 @@ require File.join('./lib', 'gilded_rose')
 describe GildedRose do
 
   describe "#update_quality" do
+    context "given Standard Item" do
+      let(:item) {Item.new("standard", 20, 20, 20)}
+      let(:gr) {GildedRose.new([item])}
+
+      before(:each) { gr.update_quality }
+
+      context "with Quality above minimum" do
+        it "does decrease quality by 1" do
+          expect(item.quality).to eq 19
+        end
+      end
+    end
   end
 end
