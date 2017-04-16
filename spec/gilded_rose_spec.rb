0273225fe5435bcdb0615a0785afd94d3cb7f56d
Doc Norton <michael@docondev.com>
2017-04-16 14:53:44 -0500

    Account for standard item after sell date

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,14 +4,24 @@ describe GildedRose do
 
   describe "#update_quality" do
     context "given Standard Item" do
-      let(:item) {Item.new("standard", 20, 20, 20)}
+      let(:item) {Item.new("standard", sell_in, quality, 20)}
       let(:gr) {GildedRose.new([item])}
 
       before(:each) { gr.update_quality }
 
       context "with Quality above minimum" do
-        it "does decrease quality by 1" do
-          expect(item.quality).to eq 19
+        let(:quality) {20}
+        context "and on or before Sell Date" do
+          let(:sell_in) {20}
+          it "does decrease quality by 1" do
+            expect(item.quality).to eq 19
+          end
+        end
+        context "and after Sell Date" do
+          let(:sell_in) {-1}
+          it "does decrease quality by 2" do
+            expect(item.quality).to eq 18
+          end
         end
       end
     end
