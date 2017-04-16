122addadd9fbd309f6fab3511a9d5a3f431dfe5e
Doc Norton <michael@docondev.com>
2017-04-16 15:05:30 -0500

    Decrease Sell In by One

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,24 +3,28 @@ require File.join('./lib', 'gilded_rose')
 describe GildedRose do
 
   describe "#update_quality" do
-    context "given Standard Item" do
-      let(:item) {Item.new("standard", sell_in, quality, 20)}
-      let(:gr) {GildedRose.new([item])}
+    before(:each) { gr.update_quality }
+    let(:gr) {GildedRose.new([item])}
+    let(:quality) {20}
+    let(:sell_in) {20}
+    let(:price) {40}
 
-      before(:each) { gr.update_quality }
+    context "given Standard Item" do
+      let(:item) {Item.new("standard", sell_in, quality, price)}
 
       context "with Quality above minimum" do
-        let(:quality) {20}
+        it "does decrease the Sell In by 1" do
+          expect(item.sell_in).to eq sell_in - 1
+        end
         context "and on or before Sell Date" do
-          let(:sell_in) {20}
-          it "does decrease quality by 1" do
-            expect(item.quality).to eq 19
+          it "does decrease Quality by 1" do
+            expect(item.quality).to eq quality - 1
           end
         end
-        context "and after Sell Date" do
-          let(:sell_in) {-1}
+        context "and on or after Sell Date" do
+          let(:sell_in) {0}
           it "does decrease quality by 2" do
-            expect(item.quality).to eq 18
+            expect(item.quality).to eq quality - 2
           end
         end
       end
