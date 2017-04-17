ffa02a3c5306b884ef8acd047bff59d62739b6b9
Doc Norton <michael@docondev.com>
2017-04-17 15:50:02 -0500

    Verified Functionality

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -14,30 +14,30 @@ describe GildedRose do
     context "given Standard Item" do
       let(:name) {"Standard"}
       context "with Quality above minimum" do
-        it "*** decreases the Sell In by 1" do
+        it "decreases the Sell In by 1" do
           expect(item.sell_in).to eq sell_in - 1
         end
         context "and before Sell Date" do
-          it "*** decreases Quality by 1" do
+          it "decreases Quality by 1" do
             expect(item.quality).to eq quality - 1
           end
-          it "*** adjusts the Price by the Quality" do
+          it "adjusts the Price by the Quality" do
             expect(gr.total).to eq item.base_price + item.quality
           end
         end
         context "and on or after Sell Date" do
           let(:sell_in) {0}
-          it "*** decreases Quality by 2" do
+          it "decreases Quality by 2" do
             expect(item.quality).to eq quality - 2
           end
-          it "*** adjusts the Price by double the days past Sell Date" do
+          it "adjusts the Price by double the days past Sell Date" do
             expect(gr.total).to eq item.base_price + item.sell_in * 2
           end
         end
 
         context "With Quality at minimum" do
           let(:quality) { 0 }
-          it "*** *** doesn't adjust quality" do
+          it "doesn't adjust quality" do
             expect(item.quality).to eq quality
           end
         end
@@ -45,10 +45,10 @@ describe GildedRose do
 
       context "with Quality at minimum" do
         let(:quality) {0}
-        it "*** doesn't adjust Quality" do
+        it "doesn't adjust Quality" do
           expect(item.quality).to eq 0
         end
-        it "*** sets price at Base" do
+        it "sets price at Base" do
           expect(gr.total).to eq item.base_price
         end
       end
@@ -57,27 +57,27 @@ describe GildedRose do
     # Aged Items
     context "given an Aged Item" do
       let(:name) {"Aged Brie"}
-      it "*** decreases the Sell In by 1" do
+      it "decreases the Sell In by 1" do
         expect(item.sell_in).to eq sell_in - 1
       end
       context "with Quality less than maximum" do
         context "and before Sell Date" do
-          it "*** increases Quality by one" do
+          it "increases Quality by one" do
             expect(item.quality).to eq quality + 1
           end
         end
         context "and on or after Sell Date" do
           let(:sell_in) {-1}
-          it "*** increases Quality by two" do
+          it "increases Quality by two" do
             expect(item.quality).to eq quality + 2
           end
           context "and near maximum Quality" do
             let(:quality) {49}
-            it "*** doesn't let Quality exceed maximum" do
+            it "doesn't let Quality exceed maximum" do
               expect(item.quality).to eq 50
             end
           end
-          it "*** Decreases price by twice Sell In Days" do
+          it "Decreases price by twice Sell In Days" do
             expect(gr.total).to eq item.base_price + item.sell_in * 2
           end
         end
@@ -85,7 +85,7 @@ describe GildedRose do
 
       context "with Quality at maximum" do
         let(:quality) {50}
-        it "*** doesn't increase Quality" do
+        it "doesn't increase Quality" do
           expect(item.quality).to eq quality
         end
       end
@@ -94,18 +94,18 @@ describe GildedRose do
     # Backstage Passes
     context "given Passes" do
       let(:name) {"Backstage passes to a TAFKAL80ETC concert"}
-      it "*** decreases the Sell In by 1" do
+      it "decreases the Sell In by 1" do
         expect(item.sell_in).to eq sell_in - 1
       end
       context "with Quality at maximum" do
         let(:quality) {50}
-        it "*** doesn't increase Quality" do
+        it "doesn't increase Quality" do
           expect(item.quality).to eq quality
         end
       end
       context "with Quality near maximum" do
         let(:quality) {49}
-        it "*** doesn't increase Quality above maximum" do
+        it "doesn't increase Quality above maximum" do
           expect(item.quality).to eq 50
         end
       end
@@ -113,52 +113,52 @@ describe GildedRose do
       context "with Quality less than maximum" do
         context "and long before Sell Date" do
           let(:sell_in) {25}
-          it "*** increases Quality by 1" do
+          it "increases Quality by 1" do
             expect(item.quality).to eq quality + 1
           end
-          it "*** adjusts the Price by the Quality" do
+          it "adjusts the Price by the Quality" do
             expect(gr.total).to eq item.base_price + item.quality
           end
         end
         context "and fairly near Sell Date" do
           let(:sell_in) {10}
-          it "*** increases Quality by 2" do
+          it "increases Quality by 2" do
             expect(item.quality).to eq quality + 2
           end
-          it "*** adjusts the Price by twice the Quality" do
+          it "adjusts the Price by twice the Quality" do
             expect(gr.total).to eq item.base_price + item.quality * 2
           end
           context "and Quality near maximum" do
             let(:quality) {49}
-            it "*** doesn't increase Quality above maximum" do
+            it "doesn't increase Quality above maximum" do
               expect(item.quality).to eq 50
             end
           end
         end
         context "and very near Sell Date" do
           let(:sell_in) {4}
-          it "*** increases Quality by 3" do
+          it "increases Quality by 3" do
             expect(item.quality).to eq quality + 3
           end
-          it "*** adjusts the Price by triple the Quality" do
+          it "adjusts the Price by triple the Quality" do
             expect(gr.total).to eq item.base_price + item.quality * 3
           end
           context "and Quality near maximum" do
             let(:quality) {49}
-            it "*** doesn't increase Quality above maximum" do
+            it "doesn't increase Quality above maximum" do
               expect(item.quality).to eq 50
             end
           end
         end
         context "and on Sell Date" do
           let(:sell_in) {0}
-          it "*** reduces Quality to 0" do
+          it "reduces Quality to 0" do
             expect(item.quality).to eq 0
           end
         end
         context "and after Sell Date" do
           let(:sell_in) {-2}
-          it "*** reduces Quality to 0" do
+          it "reduces Quality to 0" do
             expect(item.quality).to eq 0
           end
         end
@@ -171,26 +171,26 @@ describe GildedRose do
 
       context "with Initial Quality of 30" do
         let(:quality) {30}
-        it "*** maintains a quality of 30" do
+        it "** maintains a quality of 30" do
           expect(item.quality).to eq 30
         end
       end
 
       context "before Sell Date" do
         let(:sell_in) {20}
-        it "*** doesn't change quality" do
+        it "doesn't change quality" do
           expect(item.quality).to eq quality
         end
       end
       context "on Sell Date" do
         let(:sell_in) {0}
-        it "*** doesn't change quality" do
+        it "doesn't change quality" do
           expect(item.quality).to eq quality
         end
       end
       context "after Sell Date" do
         let(:sell_in) {-2}
-        it "*** doesn't change quality" do
+        it "doesn't change quality" do
           expect(item.quality).to eq quality
         end
       end
@@ -205,16 +205,16 @@ describe GildedRose do
         ]
       }
 
-      it "*** reduces normal quality" do
+      it "reduces normal quality" do
         expect(items[0].quality).to eq quality - 1
       end
-      it "*** reduces normal sell in" do
+      it "reduces normal sell in" do
         expect(items[0].sell_in).to eq sell_in - 1
       end
-      it "*** increases aged quality" do
+      it "increases aged quality" do
         expect(items[1].quality).to eq quality + 1
       end
-      it "*** reduces aged sell in" do
+      it "reduces aged sell in" do
         expect(items[1].sell_in).to eq sell_in - 1
       end
     end
