dd6326e101f1004e47d819dd68f6d302312d1aab
Doc Norton <michael@docondev.com>
2017-04-17 06:16:51 -0500

    Add pricing tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -7,7 +7,7 @@ describe GildedRose do
     let(:gr) {GildedRose.new([item])}
     let(:quality) {20}
     let(:sell_in) {20}
-    let(:price) {40}
+    let(:price) {450}
     let(:item) {Item.new(name, sell_in, quality, price)}
 
     # Standard Items
@@ -21,11 +21,17 @@ describe GildedRose do
           it "does decrease Quality by 1" do
             expect(item.quality).to eq quality - 1
           end
+          it "does adjust the Price by the Quality" do
+            expect(gr.total).to eq item.base_price + item.quality
+          end
         end
         context "and on or after Sell Date" do
           let(:sell_in) {0}
           it "does decrease Quality by 2" do
             expect(item.quality).to eq quality - 2
+          end
+          it "does adjust the Price by double the days past Sell Date" do
+            expect(gr.total).to eq item.base_price + item.sell_in * 2
           end
         end
       end
@@ -34,6 +40,9 @@ describe GildedRose do
         let(:quality) {0}
         it "does not decrease Quality" do
           expect(item.quality).to eq 0
+        end
+        it "does set price at Base" do
+          expect(gr.total).to eq item.base_price
         end
       end
     end
@@ -78,41 +87,52 @@ describe GildedRose do
       it "does decrease the Sell In by 1" do
         expect(item.sell_in).to eq sell_in - 1
       end
-      context "and at maximum Quality" do
+      context "with Quality at maximum" do
         let(:quality) {50}
         it "does not increase Quality" do
           expect(item.quality).to eq quality
         end
       end
 
-      context "long before Sell Date" do
-        let(:sell_in) {25}
-        it "does increase Quality by 1" do
-          expect(item.quality).to eq quality + 1
+      context "with Quality less than maximum" do
+        context "and long before Sell Date" do
+          let(:sell_in) {25}
+          it "does increase Quality by 1" do
+            expect(item.quality).to eq quality + 1
+          end
+          it "does adjust the Price by the Quality" do
+            expect(gr.total).to eq item.base_price + item.quality
+          end
         end
-      end
-      context "fairly near Sell Date" do
-        let(:sell_in) {10}
-        it "does increase Quality by 2" do
-          expect(item.quality).to eq quality + 2
+        context "and fairly near Sell Date" do
+          let(:sell_in) {10}
+          it "does increase Quality by 2" do
+            expect(item.quality).to eq quality + 2
+          end
+          it "does adjust the Price by twice the Quality" do
+            expect(gr.total).to eq item.base_price + item.quality * 2
+          end
         end
-      end
-      context "very near Sell Date" do
-        let(:sell_in) {4}
-        it "does increase Quality by 3" do
-          expect(item.quality).to eq quality + 3
+        context "and very near Sell Date" do
+          let(:sell_in) {4}
+          it "does increase Quality by 3" do
+            expect(item.quality).to eq quality + 3
+          end
+          it "does adjust the Price by triple the Quality" do
+            expect(gr.total).to eq item.base_price + item.quality * 3
+          end
         end
-      end
-      context "on Sell Date" do
-        let(:sell_in) {0}
-        it "does reduce Quality to 0" do
-          expect(item.quality).to eq 0
+        context "and on Sell Date" do
+          let(:sell_in) {0}
+          it "does reduce Quality to 0" do
+            expect(item.quality).to eq 0
+          end
         end
-      end
-      context "after Sell Date" do
-        let(:sell_in) {-2}
-        it "does reduce Quality to 0" do
-          expect(item.quality).to eq 0
+        context "and after Sell Date" do
+          let(:sell_in) {-2}
+          it "does reduce Quality to 0" do
+            expect(item.quality).to eq 0
+          end
         end
       end
     end
